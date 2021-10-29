453f1fa6f8524b932e51dd2961dd08b9c6f40565
Dan Voronov <dan@voronov>
2021-10-29 22:21:24 +0300

    add Славутич and ICTV to stop list

--- src/getRSS.js
+++ src/getRSS.js
@@ -5,7 +5,7 @@ let TimeAgo = require('javascript-time-ago')
 TimeAgo.addDefaultLocale(require('javascript-time-ago/locale/ru'))
 const timeAgo = new TimeAgo('ru-RU')
 
-const onlyKyiv = news => news.filter(({title})=>!title.includes('Львов')&&!title.includes('Винниц')&&!title.includes('Харьков')&&!title.includes('Одесс')&&!title.includes('Николаев')&&!title.includes('Мариуполь')&&!title.includes('Полтав')&&!title.includes('Чернигов')&&!title.includes('Тернопол'))
+const onlyKyiv = news => news.filter(({title})=>!title.includes('Львов')&&!title.includes('Винниц')&&!title.includes('Харьков')&&!title.includes('Одесс')&&!title.includes('Николаев')&&!title.includes('Мариуполь')&&!title.includes('Полтав')&&!title.includes('Чернигов')&&!title.includes('Тернопол')&&!title.includes('Славутич'))
 
 module.exports.getBZHrss = async t => {
   console.log('Getting RSS from bzh.life/feed')
